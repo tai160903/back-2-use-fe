6c2d76d66ee3c7269c93cc6d5d12b7bfe617b5e7
Cduy11 <cduy527@gmail.com>
2025-09-25 23:13:28 +0700

    create ui transaction

--- src/pages/Home/TransactionHistory/TransactionHistory.jsx
+++ src/pages/Home/TransactionHistory/TransactionHistory.jsx
@@ -17,6 +17,8 @@ import { MdOutlineQrCode2 } from "react-icons/md";
 import { FiBox } from "react-icons/fi";
 import { RiCalendarScheduleLine } from "react-icons/ri";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { CiWarning } from "react-icons/ci";
+import { MdOutlineFeedback } from "react-icons/md";
 
 // ================== Fake Data ==================
 const transactions = [
@@ -27,11 +29,11 @@ const transactions = [
       "https://www.nguyenlieutrasua.com/cdn/shop/products/D5A9827_large.jpg?v=1596429456",
     qr: "QR002",
     material: "Stainless Steel",
-    date: "22/1/2024",
-    due: "24/1/2024",
-    type: "Borrow",
+    date: "22/9/2025",
+    due: "30/1/2025",
+    type: "Return Failed",
     status: "failed",
-    overdueDays: 611,
+    overdueDays: 5,
     fee: 1833,
     deposit: -5,
     count: 3,
@@ -43,12 +45,14 @@ const transactions = [
     material: "Bamboo Fiber",
     date: "20/1/2024",
     due: "22/1/2024",
-    type: "return",
-    status: "success",
+    type: "Return Success",
+    status: "complete",
     deposit: -3,
     receive: 3,
     reward: 15,
     count: 2,
+    fee: 1833,
+    overdueDays: 5,
   },
   {
     id: 3,
@@ -57,10 +61,12 @@ const transactions = [
     material: "Plastic",
     date: "21/1/2024",
     due: "23/1/2024",
-    type: "borrow",
-    status: "success",
+    type: "Borrow",
+    status: "complete",
     deposit: -2,
     count: 1,
+    overdueDays: 5,
+    fee: 1833,
   },
 ];
 
@@ -73,9 +79,11 @@ function BorrowCard({ item }) {
           <Typography className="borrow-content-type">
             <FaArrowUpLong className="borrow-content-icons" /> {item.type}
           </Typography>
-          <Typography className="borrow-content-status">
-            {item.status}
-          </Typography>
+          <div className="borrow-content-status-wrapper">
+            <Typography className="borrow-content-status">
+              {item.status}
+            </Typography>
+          </div>
         </div>
         <div className="borrow-content">
           <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -94,33 +102,43 @@ function BorrowCard({ item }) {
                   <MdOutlineQrCode2 /> {item.qr}
                 </Typography>
                 <Typography variant="body2" className="borrow-content-material">
-                  <FiBox />
-                  Material: {item.material}
+                  <FiBox /> Material: {item.material}
                 </Typography>
                 <div className="borrow-content-time">
                   <Typography
                     variant="body2"
                     className="borrow-content-material"
                   >
-                    {" "}
-                    <RiCalendarScheduleLine />
-                    Borrowed: {item.due}
+                    <RiCalendarScheduleLine /> Borrowed: {item.date}
                   </Typography>
                   <Typography
                     variant="body2"
                     className="borrow-content-material"
                   >
-                    {" "}
-                    <RiCalendarScheduleLine />
-                    Due: {item.due}
+                    <RiCalendarScheduleLine /> Due: {item.due}
                   </Typography>
+                </div>
+                {/* Overdue đưa ngay dưới info */}
+
+                <div className="borrow-content-overdue">
+                  <Typography className="borrow-content-overdue-title">
+                    <CiWarning style={{ marginRight: "10px" }} /> Overdue by{" "}
+                    {item.overdueDays} days <br />
+                  </Typography>
+                  <Typography>Total charge fee: ${item.fee}</Typography>
                 </div>
               </div>
             </div>
             <div className="borrow-content-right">
-              <Typography>Deposit: {item.deposit}$</Typography>
+              <Typography>
+                Deposit:{" "}
+                <span style={{ color: "#cc3500", fontWeight: "bold" }}>
+                  {item.deposit} $
+                </span>
+              </Typography>
               <Button className="borrow-content-btn">
-                <MdOutlineRemoveRedEye style={{ fontSize: "20px" }} /> View Details
+                <MdOutlineRemoveRedEye style={{ fontSize: "20px" }} /> View
+                Details
               </Button>
             </div>
           </div>
@@ -132,35 +150,228 @@ function BorrowCard({ item }) {
 
 function SuccessCard({ item }) {
   return (
-    <Box className="success-card" p={2} mb={2} borderRadius="10px">
-      <Typography variant="h6">{item.name}</Typography>
-      <Typography variant="body2">QR: {item.qr}</Typography>
-      <Typography variant="body2">Material: {item.material}</Typography>
-      <Typography variant="body2">Borrowed: {item.date}</Typography>
-      <Typography variant="body2">Due: {item.due}</Typography>
-      <Typography>Deposit: {item.deposit}$</Typography>
-      {item.receive && <Typography>Receive: {item.receive}$</Typography>}
-      {item.reward && <Typography>Reward Points: {item.reward}</Typography>}
-      <Button variant="contained" color="success" size="small" sx={{ mt: 1 }}>
-        Complete
-      </Button>
+    <Box className="borrow-card-success" p={2} mb={2} borderRadius="10px">
+      <div className="borrow-container">
+        <div className="borrow-content-title">
+          <Typography className="borrow-content-type-success">
+            <FaArrowUpLong className="borrow-content-icons" /> {item.type}
+          </Typography>
+          <div className="borrow-content-status-wrapper">
+            <Typography className="borrow-content-status">
+              {item.status}
+            </Typography>
+          </div>
+        </div>
+        <div className="borrow-content">
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div className="borrow-content-info">
+              <img
+                src={item.image || "https://via.placeholder.com/150"}
+                alt={item.name}
+                className="borrow-content-image"
+              />
+              <div style={{ marginLeft: "20px", color: "#8c8987" }}>
+                <Typography variant="h6" className="borrow-content-name">
+                  {item.name}
+                  <div className="borrow-content-count">{item.count}</div>
+                </Typography>
+                <Typography variant="body2" className="borrow-content-qr">
+                  <MdOutlineQrCode2 /> {item.qr}
+                </Typography>
+                <Typography variant="body2" className="borrow-content-material">
+                  <FiBox /> Material: {item.material}
+                </Typography>
+                <div className="borrow-content-time">
+                  <Typography
+                    variant="body2"
+                    className="borrow-content-material"
+                  >
+                    <RiCalendarScheduleLine /> Borrowed: {item.date}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    className="borrow-content-material"
+                  >
+                    <RiCalendarScheduleLine /> Due: {item.due}
+                  </Typography>
+                </div>
+                {/* Overdue đưa ngay dưới info */}
+
+                <div className="borrow-content-overdue-success">
+                  <Typography className="borrow-content-overdue-title">
+                    <CiWarning style={{ marginRight: "10px" }} /> Late return
+                    but within allowed time{" "}
+                  </Typography>
+                  <div style={{ display: "flex" }}>
+                    <Typography>
+                      <Typography>Late fee: $2.00 | </Typography>
+                    </Typography>
+                    <Typography> Total returned: ${item.fee}</Typography>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="borrow-content-right-success">
+              <Typography sx={{ marginLeft: "10px" }}>
+                Deposit:{" "}
+                <span style={{ color: "#cc3500", fontWeight: "bold" }}>
+                  {item.deposit} $
+                </span>
+              </Typography>
+              <Typography sx={{ marginLeft: "10px", marginTop: "10px" }}>
+                Late fee:{" "}
+                <span style={{ color: "#cc3500", fontWeight: "bold" }}>
+                  {item.deposit} $
+                </span>
+              </Typography>
+
+              <div className="borrow-receivePoint">
+                <Typography>
+                  Receive Money: {}
+                  <span style={{ color: "#36c775", fontWeight: "bold" }}>
+                    {item.deposit} $
+                  </span>
+                </Typography>
+              </div>
+              <div className="borrow-rewardPoint">
+                <Typography>
+                  Reward Points: {}
+                  <span style={{ color: "#365bbf", fontWeight: "600" }}>
+                    + {}15 points
+                  </span>
+                </Typography>
+              </div>
+              <div className="borrow-legitPoint">
+                <Typography>
+                  Legit Points: {}
+                  <span style={{ color: "#8200de", fontWeight: "600" }}>
+                    + {}30 points
+                  </span>
+                </Typography>
+              </div>
+
+              <div style={{ display: "flex", gap: "10px" }}>
+                <Button className="borrow-content-btn">
+                  <MdOutlineFeedback style={{ fontSize: "20px" }} /> Feedback
+                </Button>
+                <Button className="borrow-content-btn">
+                  <MdOutlineRemoveRedEye style={{ fontSize: "20px" }} /> View
+                  Details
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Box>
   );
 }
 
 function FailedCard({ item }) {
   return (
-    <Box className="failed-card" p={2} mb={2} borderRadius="10px">
-      <Typography variant="h6">{item.name}</Typography>
-      <Typography variant="body2">QR: {item.qr}</Typography>
-      <Typography variant="body2">Material: {item.material}</Typography>
-      <Typography variant="body2">Borrowed: {item.date}</Typography>
-      <Typography variant="body2">Due: {item.due}</Typography>
-      <Typography color="error">Failed transaction</Typography>
-      <Typography>Deposit: {item.deposit}$</Typography>
-      <Button variant="contained" color="error" size="small" sx={{ mt: 1 }}>
-        Retry
-      </Button>
+    <Box className="borrow-card-failed" p={2} mb={2} borderRadius="10px">
+      <div className="borrow-container">
+        <div className="borrow-content-title">
+          <Typography className="borrow-content-type">
+            <FaArrowUpLong className="borrow-content-icons" /> {item.type}
+          </Typography>
+          <div className="borrow-content-status-wrapper">
+            <Typography className="borrow-content-status-failed">
+              {item.status}
+            </Typography>
+          </div>
+        </div>
+        <div className="borrow-content">
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div className="borrow-content-info">
+              <img
+                src={item.image || "https://via.placeholder.com/150"}
+                alt={item.name}
+                className="borrow-content-image"
+              />
+              <div style={{ marginLeft: "20px", color: "#8c8987" }}>
+                <Typography variant="h6" className="borrow-content-name">
+                  {item.name}
+                  <div className="borrow-content-count">{item.count}</div>
+                </Typography>
+                <Typography variant="body2" className="borrow-content-qr">
+                  <MdOutlineQrCode2 /> {item.qr}
+                </Typography>
+                <Typography variant="body2" className="borrow-content-material">
+                  <FiBox /> Material: {item.material}
+                </Typography>
+                <div className="borrow-content-time">
+                  <Typography
+                    variant="body2"
+                    className="borrow-content-material"
+                  >
+                    <RiCalendarScheduleLine /> Borrowed: {item.date}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    className="borrow-content-material"
+                  >
+                    <RiCalendarScheduleLine /> Due: {item.due}
+                  </Typography>
+                </div>
+                {/* Overdue đưa ngay dưới info */}
+
+                <div className="borrow-content-overdue-failed">
+                  <Typography className="borrow-content-overdue-title">
+                    <CiWarning style={{ marginRight: "10px" }} /> Late return
+                    but within allowed time{" "}
+                  </Typography>
+                  <div style={{ display: "flex" }}>
+                    <Typography>
+                      <Typography>Late fee: $2.00 | </Typography>
+                    </Typography>
+                    <Typography> Total returned: ${item.fee}</Typography>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="borrow-content-right-success">
+              <Typography sx={{ marginLeft: "10px" }}>
+                Deposit:{" "}
+                <span style={{ color: "#cc3500", fontWeight: "bold" }}>
+                  {item.deposit} $
+                </span>
+              </Typography>
+
+              <div className="borrow-receivePoint">
+                <Typography>
+                  Receive Money: {}
+                  <span style={{ color: "#c64b4f", fontWeight: "bold" }}>
+                    {item.deposit} $
+                  </span>
+                </Typography>
+              </div>
+
+              <div className="borrow-legitPoint-failed">
+                <Typography>
+                  Legit Points: {}
+                  <span style={{ color: "#df4d56", fontWeight: "600" }}>
+                    - {}30 points
+                  </span>
+                </Typography>
+              </div>
+              <div className="borrow-failedPoint">
+                <Typography>No refund due to failed return</Typography>
+              </div>
+
+              <div style={{ display: "flex", gap: "10px" }}>
+                <Button className="borrow-content-btn">
+                  <MdOutlineFeedback style={{ fontSize: "20px" }} /> Feedback
+                </Button>
+                <Button className="borrow-content-btn">
+                  <MdOutlineRemoveRedEye style={{ fontSize: "20px" }} /> View
+                  Details
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Box>
   );
 }
@@ -176,9 +387,11 @@ export default function TransactionHistory() {
 
   const getFilteredData = () => {
     if (value === 0) return transactions;
-    if (value === 1) return transactions.filter((t) => t.type === "borrow");
-    if (value === 2) return transactions.filter((t) => t.status === "success");
-    if (value === 3) return transactions.filter((t) => t.status === "failed");
+    if (value === 1) return transactions.filter((t) => t.type === "Borrow");
+    if (value === 2)
+      return transactions.filter((t) => t.type === "Return Success");
+    if (value === 3)
+      return transactions.filter((t) => t.type === "Return Failed");
     return transactions;
   };
 
@@ -261,11 +474,11 @@ export default function TransactionHistory() {
         {/* Render Cards */}
         <div className="transaction-list">
           {filteredData.map((item) => {
-            if (item.type === "borrow")
+            if (item.type === "Borrow")
               return <BorrowCard key={item.id} item={item} />;
-            if (item.status === "success")
+            if (item.type === "Return Success")
               return <SuccessCard key={item.id} item={item} />;
-            if (item.status === "failed")
+            if (item.type === "Return Failed")
               return <FailedCard key={item.id} item={item} />;
             return null;
           })}
