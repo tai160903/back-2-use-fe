b6d7ec557c8f282e84032347e0ff373b4c9d7031
TIEN-NEF\ADMIN <nthanhtien03@gmail.com>
2025-11-13 15:30:18 +0700

    update store direction design

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -148,8 +148,10 @@ export default function MapView({
       style={{ height: "100%", width: "100%" }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        subdomains="abcd"
+        maxZoom={19}
       />
 
       {/* Map Controller để điều khiển map khi selectedStore thay đổi */}
